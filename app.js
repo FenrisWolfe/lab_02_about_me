edfe6034ce71a70dc377f342d22e17d6076b82a5
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-05-10 00:02:17 -0700

    inoitial commit for about me page

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+/*This is the code that will ask for question input, either by typing in an
+answer or clicing ok or cancel as a yes or no prompt and return a response to
+the user using their own name. This*/
+var userName = prompt(' Hey there! What is your Name? ');
+//this will log the users name to the console
+console.log('username is', userName);
+alert('Hello, '+ userName + '. Welcome to my About me Page.');
