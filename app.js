d2ed706c925211b8fbc692bfb8356368e7d109ba
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-05-10 00:08:01 -0700

    added name input age guess and years served guess to app.js

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ var userName = prompt(' Hey there! What is your Name? ');
 //this will log the users name to the console
 console.log('username is', userName);
 alert('Hello, '+ userName + '. Welcome to my About me Page.');
+//questions about me start here.
+// asks if I served in the Navy
+var military = prompt(' Do you think I served in the Navy? ');
+if (military.trim().toLowerCase() == 'yes'){
+  alert('That is right ' + userName + ' I served in the United States Navy. Next you get to guess how many years I served.  Good Luck!');
+}else{
+  alert('Aww, nope that is wrong, I served in the United States Navy.');
+}
+//asksuser to guess my time in the service and returns an answer
+var yearsServed = prompt(' How many years do you think I served? Hint, it is between 20 and 30.');
+if (yearsServed <= 23){
+  alert(' Sorry ' + userName + ' I served over 24! ');
+}else {
+  if(yearsServed >= 25){
+    alert('Nope! too high! Lol');
+  }else{
+    alert('Hot Damn, you got it!');
+    }
+}
