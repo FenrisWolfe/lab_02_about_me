738a24747e18ed5c5136050b49c1444a44abf525
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-11 15:31:54 -0700

    We made the first question into a function!!

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 /*This is the code that will ask for question input, either by typing in an
 answer or clicing ok or cancel as a yes or no prompt and return a response to
 the user using their own name. This*/
-var userName = prompt(' Hey there! What is your Name? ');
-//this will log the users name to the console
-console.log('username is', userName);
-
-//return a greeting with the users name
-alert('Hello, '+ userName + '. Welcome to my About me Page.');
+var userName = function (name) {
+  return prompt(' Hey there! What is your Name? ');
+  console.log('username is', userName());
+  //this will log the users name to the console
+}
+// return a greeting with the users name
+alert('Hello, '+ userName(), + '. Welcome to my About me Page.');
 
 //questions about me start here.
 // asks if I served in the Navy
