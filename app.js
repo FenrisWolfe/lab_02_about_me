239da3ec32818f1295a72496a69907366385b32c
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-05-14 21:50:22 -0700

    Deployable but still need to fix number guess and linter error

--- app.js
+++ app.js
@@ -3,53 +3,46 @@
 /*This is the code that will ask for question input, either by typing in an
 answer or clicing ok or cancel as a yes or no prompt and return a response to
 the user using their own name. This*/
-var userNameAnswer = prompt(' Hey there! What is your Name? ');
+var userNameAnswer = prompt('Hey there! What is your Name?');
+//this will log the users name to the console
 console.log('username is', userNameAnswer);
-alert('Hello, '+ userNameAnswer + '. Welcome to my About me Page.');
-
-  //this will log the users name to the console
-
 // return a greeting with the users name
+alert('Hello, '+ userNameAnswer + ', Welcome to my About me Page.');
 
 //questions about me start here.
-// asks if I served in the Navy
-var militaryService = function () {
-  var answer = prompt(' Do you think I served in the Navy? Answer with a Yes or No. ');
+// asks if I served in the Military
+var militaryService = function() {
+  var answer = prompt(' Do you think I served in the Military? Answer with a Yes or No.');
+  // logs the users response to the console
   console.log('Military Service guess', answer);
-  if (answer.trim().toLowerCase() === 'yes'){
-    alert('That is right ' + userNameAnswer + ' I served in the United States Navy. Next you get to guess how many years I served.  Good Luck!');
+
+  if (answer.trim().toLowerCase() === 'yes' && 'y'){
+    alert('That is right ' + userNameAnswer + ' I served in the United States Navy. Next you get to guess whether I deployed.  Good Luck!');
   } else {
     alert('Actually, I served in the United States Navy.');
   }
-  alert(' you said ' + answer + ' to my military service');
+  alert('you said ' + answer + ' to my military service');
   return answer;
 }
 
-
-
-// asksuser to guess my time in the service and returns an answer
-var yearsServed = function () {
-  var answer = prompt(' How many years do you think I served? Hint, it is between 20 and 30.');
+// asks user to guess if I deployed overseas and returns an answer
+var dePloyed = function() {
+  var answer = prompt('Do you think I deployed overseas?');
+  // logs the users response to the console
   console.log('Years Served Guess' + answer);
-  alert(' you think I did ' + answer + ' years of service. I did over 24');
 
-  if (answer <= 23){
-    alert(' Sorry, ' + userNameAnswer + ' I served over 24! ');
-    return answer;
+  if (answer.trim().toLowerCase() === 'yes' && 'y'){
+    alert(userNameAnswer + ' you think I did deploy overseas, I did.');
   } else {
-    if(answer >= 25){
-      alert('Nope! too high! I only served just over 24 years.');
-      return answer;
-    } else {
-      alert('Hot Damn, you got it!');
-      return answer;
-    }
+    alert('Nope! Wrong answer! I actually deployed several times overseas.');
+    return answer;
   }
 }
-//asks user if they think I like dogs
 
-var kaNine = function () {
+//asks user if they think I like dogs
+var kaNine = function() {
   var dogLover = confirm(' do you think I like dogs? Click OK for Yes, Cancel for No.');
+  // logs the users response to the console
   console.log(dogLover);
   alert('you guessed my dog lover status as ' + dogLover);
 
@@ -63,56 +56,55 @@ var kaNine = function () {
 }
 
 // // asks the user to guess my age
-var age = function () {
-  var answer = prompt(' Now for a more difficult question. How old do you think I am? ');
+var age = function() {
+  var answer = confirm(' Now for a more difficult question. Do I look old enough to drink?');
   console.log(answer);
-  alert('you guessed my age as ' + answer);
+  alert('you guessed ' + answer);
 
-  if(answer <= 50){
-    alert(' Aww, you\'re too kind! but my age is actually 51.');
+  if(answer === true){
+    alert(' That\'s right!');
     return answer;
   }else {
-    if(answer >= 52){
-      alert('Oh Come On! I\'m only 51');
-      return answer;
-    }else {
-      if(answer === 51){
-        alert(' That\'s right!');
-        return answer;
-      }
-    }
+    alert('Oh Come On! I\'m Like, 51');
+    return answer;
   }
 }
 
-var quirky = function () {
+var quirky = function() {
   var answer = confirm('One last thing, Click OK if you think I am Quirky or Cancel if not. ');
+  // logs the users response to the console
   console.log('Quirky Status is: ' + answer);
   alert('You think my Quirky status is: ' + answer + ', truth be told, I am but so is JavaScript! But it\'s FUN!');
   return answer;
 }
 // This will display the alerts / answers to the prompts and confirms
 var military = militaryService();
-var years = yearsServed();
+var years = dePloyed();
 var dogs = kaNine();
 var old = age();
 var fun = quirky();
 
 //this concatenates all the questions into one alert and displays it to the user
 alert('so you guessed, ' + military + ' to the question of did I serve, ' + years + ' is your guess to how many years I served, ' + ' my dog lover status as ' + dogs + ' which in fact I do, and ' + old + ' is the age that you guessed I am. I am 51 BTW.');
-  if (fun === true){
-    alert('Oh yes, and you think I am Quirky!');
-  } else {
-    alert('Oh yes, and you actually think I am NOT Quirky when I actually am!')
-  }
+if (fun === true){
+  alert('Oh yes, and you think I am Quirky!');
+} else {
+  alert('Oh yes, and you actually think I am NOT Quirky when I actually am!')
+}
 
-  //sixth question added giving user four tries to guess a number
-  var guessing = function () {
-    var number;
-    var counter = 0;
+//sixth question added giving user four tries to guess a number
+var guessing = function() {
+  var number;
+  var counter = 0;
+  var done = true;
+  while (number !== 6 && counter < 6 && done) {
+    number = parseInt(prompt('what number am I guessing between 1 and 10? You have SIX tries'));
+    console.log(number);
+    if (counter > 6 ) {
+      done = false;
+      return done;
+    } else {
 
-    while (number !== 6 && counter < 6) {
-      number = parseInt(prompt('what number am I guessing between 1 and 10? You have SIX tries'));
-      console.log(number);
       if (number <= 5) {
         alert('you guessed too low');
         counter++;
@@ -129,33 +121,36 @@ alert('so you guessed, ' + military + ' to the question of did I serve, ' + year
         alert('that\'s Right!');
         console.log('counter' + counter);
       } else if (counter > 6) {
-        alert ('Sorry you have run out of guesses');
-        return number;
+          alert ('Sorry you have run out of guesses');
+          return number;
+        }
       }
     }
   }
   guessing();
 
-// // seventh question added, asking wat type of books I like
-var guessBooks = function () {
-var books = ['SCI-FI', 'FANTASY', 'MYSTERY', 'FICTION'];
-var whatBooks = prompt('What kind of books do you think I like?');
-console.log('whatbook Guess: ' + whatBooks);
-var flag;
+  // // seventh question added, asking wat type of books I like
+  var guessBooks = function() {
+    var books = ['SCI-FI', 'FANTASY', 'MYSTERY', 'FICTION', 'WESTERNS'];
+    var whatBooks = prompt('What kind of books do you think I like?');
+    // logs the users response to the console
+    console.log('whatbook Guess: ' + whatBooks);
+    var flag;
 
-for (var i=0; i<books.length; i++) {
-  console.log('Current books: ', books[i]);
+    for (var i=0; i<books.length; i++) {
+      // logs the users response to the console
+      console.log('Current books: ', books[i]);
 
-  if (whatBooks.trim().toUpperCase() === books[i]) {
-    alert('Good guess. That is one of my favorite kind of books.');
-    flag = true;
-    break;
+      if (whatBooks.trim().toUpperCase() === books[i]) {
+        alert('Good guess. That is one of my favorite kind of books.');
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag) {
+      alert('Not a kind of book I like, but good guess.');
+    }
   }
-}
 
-if (!flag) {
-  alert('Not a kind of book I like, but good guess.');
-}
-}
-
-guessBooks();
+  guessBooks();
